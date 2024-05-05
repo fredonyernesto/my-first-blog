19801e6ef70c6e28d4b3bfa16733d86e05623069
SHMERNIE <fredony.ernesto@gmail.com>
2024-05-05 12:09:15 -0700

    added a clear timeout function, display message function, and a conditional statement to confirm if all entries were filled and if the user would like to confirm their submission.

--- assets/script.js
+++ assets/script.js
@@ -4,20 +4,41 @@ const inputContent = document.getElementById("content")
 const submitBttn = document.getElementById("submit")
 const warningMSG = document.getElementById("warning-msg")
 
-function saveInputToLocalStorage (){
+function displayWarning(message){
+    warningMSG.style.color = "red"
+    warningMSG.style.marginTop = '10px'
+    warningMSG.innerText = message;
+}
 
-    const userInput = {
+function saveInputToLocalStorage (event){
+    event.preventDefault();
+
+    const user = {
         username: inputUsername.value.trim(),
         title: inputTitle.value.trim(),
         content: inputContent.value.trim(),
     };
 
-    localStorage.setItem('userInput', JSON.stringify(userInput));
+    localStorage.setItem('userInput', JSON.stringify(user));
     
+    if(user.username === ''){
+        displayWarning("Please fill in your username.")
+    } else if(user.title === ''){
+        displayWarning("Please add a title.")
+    } else if(user.content === ''){
+        displayWarning("Please provide content for your submission.")
+    } else{
+        setTimeout(function (){
+            let moveWindow;
+            let confirmWindow = confirm("Are you sure you want to submit this information?");
+            if(confirmWindow === false){
+                clearTimeout();
+            } else {
+                moveWindow = window.location.href="./main/main.html";
+                confirmWindow = moveWindow; 
+            }
+        },1000)
+    }
 }
 
-submitBttn.addEventListener('click', function(event){
-    event.preventDefault();
-    saveInputToLocalStorage();
-    
-}); 
+submitBttn.addEventListener('click', saveInputToLocalStorage);
