83f0e85731f0f2ec8a8548df3a2a7ed0c4a33693
SHMERNIE <fredony.ernesto@gmail.com>
2024-05-28 13:51:39 -0700

    Worked on a history.back() method, edited the displayExistingEntries() function, created a stylesheet for the blog entries, but I need to go back and finish adding a header, footer, and provide functionality to the light-mode/dark-mode button.

--- main/assets/script-main.js
+++ main/assets/script-main.js
@@ -1,21 +1,29 @@
 const existingEntries = JSON.parse(localStorage.getItem('userEntries')) || [];
 
+
 function displayExistingEntries(){
     const blogPost = document.getElementById("blog-post");
     blogPost.innerHTML = '';
     for (let i=0; i < existingEntries.length; i++){
-        const username = document.createElement('h3');
-        username.textContent = `Username: ${existingEntries[i].username}`
+        const username = document.createElement('h4');
+        username.textContent = `Posted by: ${existingEntries[i].username}`
         const title = document.createElement('h2');
         title.textContent = `Title: ${existingEntries[i].title}`
         const div = document.createElement('div');
-        const content = document.createElement('p')
-        content.textContent = `Content: ${existingEntries[i].content}`
-        div.append(username, title, content);
+        const content = document.createElement('p');
+        content.textContent = `Content: ${existingEntries[i].content}`;
+        div.classList.add('entries')
+        div.append(title, content, username);
         blogPost.append(div);
     }
 };
 
+
+
 window.onload = function(){
     displayExistingEntries();
 };
+
+document.getElementById("back-button").addEventListener("click", () => {
+    history.back();
+});
